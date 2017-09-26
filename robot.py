d0e1cb69cdc7d006d05c24414cc7c91230b966cf
FreddieStern <31613156+FreddieStern@users.noreply.github.com>
2017-09-26 16:06:04 +0200

    # Added if-sats

--- robot.py
+++ robot.py
@@ -63,22 +63,32 @@ class Product:
 		self.tax = tax
 
 	def price_with_tax(self):
-		return self.price * self.count * self.tax
+		total = self.price * self.count * self.tax
+		if total > 500:
+			return 0.9 * total
+		else:
+			return total
 
-robot = Product(price=900, count=2, tax=1.25)
-book = Product(price=100, count=1, tax=1.06)
+# robot = Product(price=900, count=2, tax=1.25)
+# book = Product(price=100, count=1, tax=1.06)
 
-print(robot.price_with_tax() + book.price_with_tax())
+# print(robot.price_with_tax() + book.price_with_tax())
 
-products = [Product(price=900,count=2,tax=1.25), Product(price=100,count=1,tax=1.06)]
-total_price = products[0].price_with_tax() + products[0].price_with_tax()
-print(total_price)
+# products = [Product(price=900,count=2,tax=1.25), Product(price=100,count=1,tax=1.06)]
+# total_price = products[0].price_with_tax() + products[0].price_with_tax()
+# print(total_price)
 
 products = [Product(price=900,count=2,tax=1.25), Product(price=100,count=1,tax=1.06)]
 total_price = 0
 for product in products: 
 	total_price = total_price + product.price_with_tax()
-print(total_price)
+print(total_price) 
+
+
+
+
+
+
 
  
 
